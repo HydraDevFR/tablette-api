b540f7ee6b599a976017cd0492c2fb7dd0db6853
HydraDev <55934542+HydraDevFR@users.noreply.github.com>
2022-01-29 01:54:15 +0100

    Add files via upload

---
+++ tablette/fxmanifest.lua
@@ -0,0 +1,24 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name 'Tablette API'
+description 'Tablette IOS'
+version '1.0'
+author 'HydraDev'
+
+ui_page 'html/tablet.html'
+
+client_scripts {
+	'client.lua'
+}
+
+files {
+	'html/tablet.html',
+	'html/ipad.png',
+	'html/assets/flexslider-global.css',
+	'html/assets/bundle-print.css',
+	'html/assets/bundle.js',
+	'html/assets/bundle.css',
+	'html/assets/css/custom.css',
+	'html/assets/css/tablet.css'
+}
